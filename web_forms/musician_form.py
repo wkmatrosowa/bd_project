1d9b436b01c0a2a117dca025d320ce7eac95aace
wksmirnowa <merriel@inbox.ru>
2020-02-02 19:54:46 +0300

    update html

--- web_forms/musician_form.py
+++ web_forms/musician_form.py
@@ -1,9 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, SelectField
 
 
 class MusicianForm(FlaskForm):
     firstname = StringField('Имя')
     surname = StringField('Фамилия')
-    specialization = StringField('Специализация')
+    specialization = SelectField('Специализация', choices=[
+        ('Не выбрано', 'Не выбрано'),
+        ('Вокалист', 'Вокалист'),
+        ('Басист', 'Басист'),
+        ('Солист-гитарист', 'Солист-гитарист'),
+        ('Ударник', 'Ударник'),
+        ('Барабанщик', 'Барабанщик'),
+        ('Пианист', 'Пианист'),
+    ])
     submit = SubmitField('Сохранить')
