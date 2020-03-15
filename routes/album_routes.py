035e1461ca285182a9cd736a02c68f65985d937d
wksmirnowa <merriel@inbox.ru>
2020-03-15 13:55:45 +0300

    edit and delete albums

--- routes/album_routes.py
+++ routes/album_routes.py
@@ -18,4 +18,25 @@ def album(id_performer):
     if form.validate_on_submit():
         AlbumService().save(data=form, id_performer=id_performer)
         return redirect('/albums/{}'.format(id_performer))
-    return render_template('album.html', form=form, id_performer=id_performer)
+    return render_template('album.html', form=form, id_performer=id_performer)\
+
+@album_urls.route('/album/<id_album>/performer/<id_performer>', methods=['GET', 'POST'])
+def album_id(id_album, id_performer):
+    album_for_html = AlbumService().find(id=id_album)
+    if len(album_for_html) == 1:
+        album_for_html = album_for_html[0]
+    else:
+        return 'Oops'
+    form = AlbumForm()
+    if form.validate_on_submit():
+        AlbumService().save(data=form, id=id_album, id_performer=id_performer)
+        return redirect('/albums/{}'.format(id_performer))
+    form.albumname.data = album_for_html['albumname']
+    form.genre.data = album_for_html['genre']
+    form.year.data = album_for_html['year']
+    return render_template('album.html', form=form, id=id_album, id_performer=id_performer)
+
+@album_urls.route('/album/<id>/delete/<id_performer>')
+def album_delete(id, id_performer):
+    AlbumService().delete(id)
+    return redirect('/albums/{}'.format(id_performer))
